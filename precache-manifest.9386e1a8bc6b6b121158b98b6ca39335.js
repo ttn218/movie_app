b780338940d7098dbbd0c5271894b8cb016ba8f8
ttn218 <54901871+ttn218@users.noreply.github.com>
2020-08-25 21:25:07 +0900

    Updates

---
+++ precache-manifest.9386e1a8bc6b6b121158b98b6ca39335.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4afaad856f8e3c88af03fae8af07181",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "003b95397ab93a5a135a",
+    "url": "/movie_app/static/css/main.4e502428.chunk.css"
+  },
+  {
+    "revision": "2cb00f0b337cb1b1485d",
+    "url": "/movie_app/static/js/2.03729dac.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app/static/js/2.03729dac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "003b95397ab93a5a135a",
+    "url": "/movie_app/static/js/main.8fbf4a67.chunk.js"
+  },
+  {
+    "revision": "8a294428b8499a7b0c17",
+    "url": "/movie_app/static/js/runtime-main.9ac0a6e2.js"
+  }
+]);
